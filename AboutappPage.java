b7c8bcbc768d78960b6085290d44229b73819233
Henok0728 <henokgizaw06@gmail.com>
2026-01-27 22:06:55 +0300

    final push

---
+++ AboutappPage.java
@@ -0,0 +1,18 @@
+import javax.Swing.*;
+import java.awt.*;
+
+public class AboutappPage extends JFrame{
+    private JLabel label;
+    private JLabel label2;
+    private JPanel panel;
+     public AboutappPage(){
+        this.label = new JLabel();
+        this.label.setTitle("About App");
+        this.setLayout(new BorderLayout(30,0));
+        
+         
+            
+    }
+}
+
+
---
+++ src/main/java/com/pharmacy/inventory/ui/AboutappPage.java
@@ -0,0 +1,16 @@
+package com.pharmacy.inventory.ui;
+import javax.swing.*;
+import java.awt.*;  
+public class AboutappPage extends JPanel{
+    public AboutappPage(){
+        this.setLayout(new BorderLayout(30,0));     
+    }
+
+    public void refreshData() {
+        loadData();
+    }
+    public int loadData(){
+        return 0;
+    }
+    
+}
